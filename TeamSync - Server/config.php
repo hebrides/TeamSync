386bf586cd4f64b7f12092aa7feeafdcd745375f
marcus <marcus.skye.lewis@gmail.com>
2015-09-11 07:34:35 -0400

    Update config.php

--- TeamSync - Server/config.php
+++ TeamSync - Server/config.php
@@ -5,5 +5,5 @@ $user = "teamsync";         // input your mysql user name
 $pass = "teamsync";     // input your mysql user pasword
 $database = "teamsync"; // input your mysql database name
 /////////////////////////////////////////////////////////
-$connection_ip = "66.228.33.88";    // input your connection IP address
+$connection_ip = "12.345.67.89";    // input your connection IP address
 ?>
